c19c1084579ab6759c26b7492b174211033a33d5
Ajith <50458195+Ajithkumar48@users.noreply.github.com>
2023-04-26 11:51:35 +0300

    Add files via upload

--- telegraf_start.sh
+++ telegraf_start.sh
@@ -6,6 +6,36 @@ WORKING_DIRECTORY="$(pwd)"
 # Print the working directory path
 echo "The working directory is: $WORKING_DIRECTORY"
 
+# Get the hostname
+HOSTNAME=$(hostname)
+
+# Define the filename
+TEMPLATE_FILE="$WORKING_DIRECTORY/etc/telegraf/telegraf_template.conf"
+CONF_FILE="$WORKING_DIRECTORY/etc/telegraf/telegraf.conf"
+
+# Set the new hostname
+NEW_HOSTNAME=$(hostname)
+
+# Check if the template file exists
+if [ ! -f "$TEMPLATE_FILE" ]; then
+    echo "Template file not found: $TEMPLATE_FILE"
+    exit 1
+fi
+
+# Remove the output file if it already exists
+if [ -f "$CONF_FILE" ]; then
+    echo "Existing Config file found at $CONF_FILE and Deleted"
+    rm "$CONF_FILE"
+fi
+
+
+# Copy the contents of the template file to the output file
+sed "s/P_Hostname/$NEW_HOSTNAME/g" "$TEMPLATE_FILE" > "$CONF_FILE"
+
+# Display a message indicating success
+echo "Created $CONF_FILE from $TEMPLATE_FILE"
+
+
 # Set the service name
 SERVICE_NAME=telegraf
 
@@ -33,6 +63,3 @@ sudo systemctl daemon-reload
 # Start the service and enable it to run at boot time
 sudo systemctl start $SERVICE_NAME
 sudo systemctl enable $SERVICE_NAME
-
-
-sudo systemctl status $SERVICE_NAME -l
