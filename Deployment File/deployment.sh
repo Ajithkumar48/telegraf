922cd7a0ed2f74631919a3aa6a96afeadb0fe736
Ajith <50458195+Ajithkumar48@users.noreply.github.com>
2023-04-27 17:23:52 +0300

    updated

--- Deployment File/deployment.sh
+++ Deployment File/deployment.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#Standalone script to run the deployment from source machine [Linux] having SSH enabled
+
+SERVER="127.0.0.1"
+SSH_USER="jenkins"
+
 # Locate Source Directory Having "Telegraf" in Folder Name
 SEARCH_DIR="$(dirname "${BASH_SOURCE[0]}")/"
 FOLDER_NAME="telegraf"
@@ -18,34 +23,27 @@ fi
 
 echo "Source folder: ${SOURCE_DIR}"
 
-# Loop through each row in the CSV file
-while IFS=',' read -r SERVER SSH_USER SSH_PASS SSH_PORT; do
+# Set the destination directory with SSH_USER
+DEST_DIR="/home/${SSH_USER}/telegraf"
+echo "Destination folder: ${DEST_DIR}"
 
-  # Set the destination directory with SSH_USER
-  DEST_DIR="/home/${SSH_USER}/telegraf"
+echo ""
+echo "------------------------------------------------------------------------"
+echo "[STARTED] telegraf.service initiated on server: ${SERVER}"
+echo "------------------------------------------------------------------------"
+echo ""
 
+# Checking status of telegraf.service on current server
+if ssh -i /home/vagrant/.ssh/id_rsa -o StrictHostKeyChecking=no -p 22 "${SSH_USER}@${SERVER}" "sudo systemctl is-active telegraf>/dev/null 2>&1"; then
+  echo "The telegraf service is already running on ${SERVER}, skipping this server..."
+else
+  echo "Copying files to ${SERVER}..."
   echo ""
-  echo "------------------------------------------------------------------------"
-  echo "[STARTED] telegraf.service initiated on server: ${SERVER}"
-  echo "------------------------------------------------------------------------"
-  echo ""
+  ssh -i /home/vagrant/.ssh/id_rsa -o StrictHostKeyChecking=no -p 22 "${SSH_USER}@${SERVER}" "mkdir -p ${DEST_DIR}" > /dev/null
+  scp -i /home/vagrant/.ssh/id_rsa -o StrictHostKeyChecking=no -P 22 -r "${SOURCE_DIR}"* "${SSH_USER}@${SERVER}:${DEST_DIR}"
+  ssh -i /home/vagrant/.ssh/id_rsa -o StrictHostKeyChecking=no -p 22 "${SSH_USER}@${SERVER}" "sudo chmod -R 777 ${DEST_DIR} && cd ${DEST_DIR} && sudo ./telegraf_start.sh" > /dev/null
+fi
 
-  # Checking status of telegraf.service on current server
-  if sshpass -p "${SSH_PASS}" ssh -o StrictHostKeyChecking=no -p 22 "${SSH_USER}@${SERVER}" "sudo systemctl is-active telegraf>/dev/null 2>&1"; then
-    echo "The telegraf service is already running on ${SERVER}, skipping this server..."
-  else
-    echo "Copying files to ${SERVER}..."
-    echo ""
-    sshpass -p "${SSH_PASS}" ssh -o StrictHostKeyChecking=no -p 22 "${SSH_USER}@${SERVER}" "mkdir -p ${DEST_DIR}" > /dev/null
-    scp -o StrictHostKeyChecking=no -P 22 -r "${SOURCE_DIR}"* "${SSH_USER}@${SERVER}:${DEST_DIR}"
-    sshpass -p "${SSH_PASS}" ssh -o StrictHostKeyChecking=no -p 22 "${SSH_USER}@${SERVER}" "sudo chmod -R 777 ${DEST_DIR} && cd ${DEST_DIR} && sudo ./telegraf_start.sh" > /dev/null
-  fi
-
-  echo ""
-  echo "[Status]" && sshpass -p "${SSH_PASS}" ssh -o StrictHostKeyChecking=no -p 22 "${SSH_USER}@${SERVER}" "sudo systemctl status telegraf | grep -E 'Active:|Loaded:|Main PID:|CGroup:'"
-  echo ""
-
-done < "${SEARCH_DIR}/servers.csv"
-
-# Pause the script to allow the user to read the output
-read -rp "Press any key to continue..."
+echo ""
+echo "[Status]" && ssh -i /home/vagrant/.ssh/id_rsa -o StrictHostKeyChecking=no -p 22 "${SSH_USER}@${SERVER}" "sudo systemctl status telegraf | grep -E 'Active:|Loaded:|Main PID:|CGroup:'"
+echo ""
